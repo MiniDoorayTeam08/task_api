0e068dbd345a1c39fe97f5adc2dc155d38ec95a7
soocy0718 <soocy0718@gmail.com>
2024-01-25 13:32:49 +0900

    add entity(project, task, comment)

---
+++ src/main/java/com/nhnacademy/midoo/taskapi/entity/Projects.java
@@ -0,0 +1,37 @@
+package com.nhnacademy.midoo.taskapi.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "project")
+public class Projects {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_id")
+    private Long projectId;
+
+    @Column(name = "account_id")
+    private String accountId;
+
+    @Column(name = "project_name")
+    private String projectName;
+
+    @Column(name = "project_explain")
+    private String projectExplain;
+
+    @Column(name = "project_status")
+    private String projectStatus;
+}
